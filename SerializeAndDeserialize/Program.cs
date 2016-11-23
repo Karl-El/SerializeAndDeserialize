f2d5074d64c8cc4693c6d0f26ee6a6ca21cedaa6
KarlKrus <carl_son_cruz@live.com>
2016-11-23 10:48:07 +0800

    11.23.16.10.47.AM

--- SerializeAndDeserialize/Program.cs
+++ SerializeAndDeserialize/Program.cs
@@ -22,16 +22,65 @@ namespace SerializeAndDeserialize
                     Name = "Product Name 01",
                     CategoryName = "Category Name 01",
                     price = new Price { Value = 100, Unit = "USD" },
-                    description = new Description { Color = "red", Size = "Size 01", Weight = "100g" }
+                    description = new Description { Color = "Red", Size = "Size 01", Weight = "100g" }
                 });
+                ListProducts.Add(new Product
+                {
+                    ID = "Prod ID 02",
+                    Name = "Product Name 02",
+                    CategoryName = "Category Name 02",
+                    price = new Price { Value = 100, Unit = "EUR" },
+                    description = new Description { Color = "Blue", Size = "Size 02", Weight = "200g" }
+                });
+                ListProducts.Add(new Product
+                {
+                    ID = "Prod ID 03",
+                    Name = "Product Name 03",
+                    CategoryName = "Category Name 03",
+                    price = new Price { Value = 100, Unit = "PHP" },
+                    description = new Description { Color = "Green", Size = "Size 03", Weight = "300g" }
+                });
+                XmlSerializer xmlserializer = new XmlSerializer(typeof(List<Product>));
+                StreamWriter SW = new StreamWriter("ListOfProduct.xml");
+                xmlserializer.Serialize(SW, ListProducts);
+                SW.Close();
+                Console.WriteLine("Serialization Success");
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);
             }
 
-            /*
-            //Serialize
+            //List Deserialize
+            try
+            {
+                XmlSerializer xmlserializer = new XmlSerializer(typeof(List<Product>));
+                StreamReader SR = new StreamReader("ListOfProduct.xml");
+                List<Product> listproduct = (List<Product>)xmlserializer.Deserialize(SR);
+                Console.WriteLine("List Product Information");
+                Console.WriteLine();
+                foreach (Product product in listproduct)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("ID     : " + product.ID);
+                    Console.WriteLine("Name   : " + product.Name);
+                    Console.WriteLine("Price  : " + product.price.Value);
+                    Console.WriteLine("Unit   : " + product.price.Unit);
+                    Console.WriteLine("Color  : " + product.description.Color);
+                    Console.WriteLine("Size   : " + product.description.Size);
+                    Console.WriteLine("Weight : " + product.description.Weight);
+                    Console.WriteLine();
+                    Console.WriteLine("♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+
+
+            /*//Serialize
             try
             {
                 Product product = new Product
