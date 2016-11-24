0aa6a02e3798f06bbd491ed1275dc0c90c8d5b03
KarlKrus <carl_son_cruz@live.com>
2016-11-24 10:38:33 +0800

    11.24.16.10.38.AM

--- SerializeAndDeserializeWeb/ProductCRUD.aspx.cs
+++ SerializeAndDeserializeWeb/ProductCRUD.aspx.cs
@@ -7,18 +7,71 @@ using System.Web.UI.WebControls;
 using SerializeAndDeserialize;
 using System.Xml.Serialization;
 using System.IO;
+using System.Xml;
 
 namespace SerializeAndDeserializeWeb
 {
     public partial class ProductCRUD : System.Web.UI.Page
     {
+        string message;
+        int nodeCount = 0;
+        public void AlertClear()
+        {
+            string script = "window.onload = function(){ alert('";
+            script += message;
+            script += "');";
+            script += "window.location = '";
+            script += Request.Url.AbsoluteUri;
+            script += "'; }";
+            ClientScript.RegisterStartupScript(this.GetType(), "SuccessMessage", script, true);
+        }
+
         protected void Page_Load(object sender, EventArgs e)
         {
         }
 
         protected void _btnSave_Click(object sender, EventArgs e)
         {
-            //List Serialize
+            string XmlFile = @"C:/Users/ccruz1/Documents/visual studio 2015/Projects/SerializeAndDeserialize/SerializeAndDeserializeWeb/ProductWEB.xml";
+            if (!File.Exists(XmlFile))
+            {
+                //Serialize One Item only        
+                Product product = new Product
+                {
+                    ID = _txtID.Text.Trim(),
+                    Name = _txtName.Text.Trim(),
+                    CategoryName = _txtCategory.Text.Trim(),
+                    price = new Price { Value = Convert.ToInt32(_txtPrice.Text), Unit = _txtUnit.Text.Trim() },
+                    description = new Description { Color = _txtColor.Text.Trim(), Size = _txtSize.Text.Trim(), Weight = _txtWeight.Text.Trim() }
+                };
+                XmlSerializer xmlserializer = new XmlSerializer(typeof(Product));
+                string PathName = "C:/Users/ccruz1/Documents/visual studio 2015/Projects/SerializeAndDeserialize/SerializeAndDeserializeWeb/";
+                StreamWriter SW = new StreamWriter(PathName + "ProductWEB.xml");
+                xmlserializer.Serialize(SW, product);
+                SW.Close();
+                message = "Saved"; AlertClear();
+                //End of Serialize One Item only
+            }
+            else
+            {
+                using (var reader = XmlReader.Create("C:/Users/ccruz1/Documents/visual studio 2015/Projects/SerializeAndDeserialize/SerializeAndDeserializeWeb/ProductWEB.xml"))
+                {
+                    while (reader.Read())
+                    {
+                        if (reader.NodeType == XmlNodeType.Element &&
+                            reader.Name == "name")
+                        {
+                            nodeCount++;
+                        }
+                    }
+                }
+
+
+            }
+            message = "Saved! " + nodeCount +" Total Records" ; AlertClear();
+
+
+            /*//List Serialize
             List<Product> ListProducts = new List<Product>();
             ListProducts.Add(new Product
             {
@@ -35,18 +88,8 @@ namespace SerializeAndDeserializeWeb
             {
                 xmlserializer.Serialize(SW, ListProducts);
             }
-            SW.Close();
-
-
-            string message = "Saved";
-            string script = "window.onload = function(){ alert('";
-            script += message;
-            script += "');";
-            script += "window.location = '";
-            script += Request.Url.AbsoluteUri;
-            script += "'; }";
-            ClientScript.RegisterStartupScript(this.GetType(), "SuccessMessage", script, true);
-            //List Serizalize
+            SW.Close();*/
+            //End of List Serizalize
 
 
             /*//Serialize One Item only        
@@ -66,7 +109,7 @@ namespace SerializeAndDeserializeWeb
              SW.Close();
              ScriptManager.RegisterStartupScript(this, this.GetType(), "popup", "alert('Save');", true);
 
-            //Serialize One Item only*/
+            //End of Serialize One Item only*/
         }
     }
 }
