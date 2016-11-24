e4a1d11954006868fa721e89868b482f2d6f3eae
KarlKrus <carl_son_cruz@live.com>
2016-11-24 10:51:08 +0800

    11.24.16.10.50.AM

--- SerializeAndDeserializeWeb/ProductCRUD.aspx.cs
+++ SerializeAndDeserializeWeb/ProductCRUD.aspx.cs
@@ -25,15 +25,15 @@ namespace SerializeAndDeserializeWeb
             script += "'; }";
             ClientScript.RegisterStartupScript(this.GetType(), "SuccessMessage", script, true);
         }
+        string XmlFileFullPath = @"C:/Users/ccruz1/Documents/visual studio 2015/Projects/SerializeAndDeserialize/SerializeAndDeserializeWeb/ProductWEB.xml";
 
         protected void Page_Load(object sender, EventArgs e)
         {
         }
 
         protected void _btnSave_Click(object sender, EventArgs e)
-        {
-            string XmlFile = @"C:/Users/ccruz1/Documents/visual studio 2015/Projects/SerializeAndDeserialize/SerializeAndDeserializeWeb/ProductWEB.xml";
-            if (!File.Exists(XmlFile))
+        {;
+            if (!File.Exists(XmlFileFullPath))
             {
                 //Serialize One Item only        
                 Product product = new Product
@@ -54,21 +54,32 @@ namespace SerializeAndDeserializeWeb
             }
             else
             {
-                using (var reader = XmlReader.Create("C:/Users/ccruz1/Documents/visual studio 2015/Projects/SerializeAndDeserialize/SerializeAndDeserializeWeb/ProductWEB.xml"))
+                //Node Count
+                using (var reader = XmlReader.Create(XmlFileFullPath))
+                { while (reader.Read()) { if (reader.NodeType == XmlNodeType.Element && reader.Name == "name") { nodeCount++; } } }
+                //End of Node Count
+                if (nodeCount >= 0) { }
+                else
                 {
-                    while (reader.Read())
+                    //Serialize One Item only        
+                    Product product = new Product
                     {
-                        if (reader.NodeType == XmlNodeType.Element &&
-                            reader.Name == "name")
-                        {
-                            nodeCount++;
-                        }
-                    }
+                        ID = _txtID.Text.Trim(),
+                        Name = _txtName.Text.Trim(),
+                        CategoryName = _txtCategory.Text.Trim(),
+                        price = new Price { Value = Convert.ToInt32(_txtPrice.Text), Unit = _txtUnit.Text.Trim() },
+                        description = new Description { Color = _txtColor.Text.Trim(), Size = _txtSize.Text.Trim(), Weight = _txtWeight.Text.Trim() }
+                    };
+                    XmlSerializer xmlserializer = new XmlSerializer(typeof(Product));
+                    string PathName = "C:/Users/ccruz1/Documents/visual studio 2015/Projects/SerializeAndDeserialize/SerializeAndDeserializeWeb/";
+                    StreamWriter SW = new StreamWriter(PathName + "ProductWEB.xml");
+                    xmlserializer.Serialize(SW, product);
+                    SW.Close();
+                    message = "Saved"; AlertClear();
+                    //End of Serialize One Item only
                 }
-
-
             }
-            message = "Saved! " + nodeCount +" Total Records" ; AlertClear();
+            message = "Saved! " + nodeCount + " Total Records"; AlertClear();
 
 
             /*//List Serialize
