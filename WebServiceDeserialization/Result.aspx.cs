2e8635a6f1c0af9d9318710534f4fc7d048bae16
KarlKrus <carl_son_cruz@live.com>
2016-11-28 14:55:14 +0800

    11.28.16.02.55.PM

--- WebServiceDeserialization/Result.aspx.cs
+++ WebServiceDeserialization/Result.aspx.cs
@@ -12,18 +12,32 @@ namespace WebServiceDeserialization
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            string NodeType, NodeName;
+
             XmlTextReader reader = new XmlTextReader("http://afs-sl-schmgr03.afservice.org:8080/searchManager/search/afs-sl-schmstr.afservice.org:8080/solr4/Products/select?q=laptop&fl=EDP&store=pcmall&rows=25&start=0&facet=true&facet.field=Manufacturer&facet.field=InStock&facet.limit=10");
             reader.WhitespaceHandling = WhitespaceHandling.Significant;
-            while (reader.Read())
+            if (reader.ReadToDescendant("result"))
             {
-
-                NodeType = reader.NodeType.ToString(); NodeName = reader.Name;
-                // Print out info on node  
-                Response.Write("Node Type: " + NodeType + "</br>");
-                Response.Write("Node Name: " + NodeName + "</br></br>");
-                Response.Write("");
+                reader.Read();//this moves reader to next node which is text 
+               string  result = reader.Value; //this might give value than 
+                Response.Write("Value:" + result);
+                Response.Write("</br>");
             }
+
+
+            /* //-----------------------------------------------------------------------------------------------------WORKING READ
+            while (reader.ReadToFollowing("int"))
+            {
+                string attr = reader.GetAttribute("name");
+                string valuetext = reader.ReadElementString("int");
+
+                Response.Write("Attribute Name: "+attr);
+                Response.Write("</br>");
+                Response.Write("Value:"+valuetext);
+                Response.Write("</br>");
+                Response.Write("</br>");
+                
+            }*/
+
         }
     }
 }
