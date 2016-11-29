6c8cae1ff6d6c211caa728e8b20367c4f2a66010
KarlKrus <carl_son_cruz@live.com>
2016-11-29 11:28:52 +0800

    11.29.16.11.28.AM

--- WebServiceDeserialization/Result.aspx.cs
+++ WebServiceDeserialization/Result.aspx.cs
@@ -46,20 +46,23 @@ namespace WebServiceDeserialization
 
 
             //-----------------------------------------------------------------------------------------------------WORKING READER
-            XmlTextReader reader = new XmlTextReader("http://afs-sl-schmgr03.afservice.org:8080/searchManager/search/afs-sl-schmstr.afservice.org:8080/solr4/Products/select?q=laptop&fl=EDP&store=pcmall&rows=100&start=0&facet=true&facet.field=Manufacturer&facet.field=InStock&facet.limit=10");
+            XmlTextReader reader = new XmlTextReader("http://afs-sl-schmgr03.afservice.org:8080/searchManager/search/afs-sl-schmstr.afservice.org:8080/solr4/Products/select?q=laptop&fl=EDP&store=pcmall&rows=10&start=0&facet=true&facet.field=Manufacturer&facet.field=InStock&facet.limit=-1");
             reader.WhitespaceHandling = WhitespaceHandling.Significant;
-            while (reader.ReadToFollowing("result"))
+            while (reader.ReadToFollowing("doc"))
             {
                 while (reader.ReadToFollowing("int"))
                 {
-                    string attr = reader.GetAttribute("name");
-                    string valuetext = reader.ReadElementString("int");
+                    if (reader.GetAttribute("name") == "EDP")
+                    {
+                        string attr = reader.GetAttribute("name");
+                        string valuetext = reader.ReadElementString("int");
 
-                    Response.Write("Attribute Name: " + attr);
-                    Response.Write("</br>");
-                    Response.Write("Value:" + valuetext);
-                    Response.Write("</br>");
-                    Response.Write("</br>");
+                        //Response.Write("Attribute Name: " + attr);
+                        //Response.Write("</br>");
+                        Response.Write("Value:" + valuetext);
+                        //Response.Write("</br>");
+                        Response.Write("</br>");
+                    }
                 }
 
             }
