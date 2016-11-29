6633486b05edaea6b3b899e2db04d1c39fd9a0aa
KarlKrus <carl_son_cruz@live.com>
2016-11-29 11:37:55 +0800

    11.29.16.11.37.AM

--- WebServiceDeserialization/Result.aspx.cs
+++ WebServiceDeserialization/Result.aspx.cs
@@ -48,7 +48,7 @@ namespace WebServiceDeserialization
             //-----------------------------------------------------------------------------------------------------WORKING READER
             XmlTextReader reader = new XmlTextReader("http://afs-sl-schmgr03.afservice.org:8080/searchManager/search/afs-sl-schmstr.afservice.org:8080/solr4/Products/select?q=laptop&fl=EDP&store=pcmall&rows=10&start=0&facet=true&facet.field=Manufacturer&facet.field=InStock&facet.limit=-1");
             reader.WhitespaceHandling = WhitespaceHandling.Significant;
-            while (reader.ReadToFollowing("doc"))
+            while (reader.ReadToFollowing("result"))
             {
                 while (reader.ReadToFollowing("int"))
                 {
