b950ed29bc3520ddcae03d8dd4399a86c2c24d56
KarlKrus <carl_son_cruz@live.com>
2016-11-29 17:43:28 +0800

    11.29.16.05.43.PM

--- WebServiceDeserialization/ProductDetails.aspx.cs
+++ WebServiceDeserialization/ProductDetails.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Xml;
 
 namespace WebServiceDeserialization
 {
@@ -11,8 +12,27 @@ namespace WebServiceDeserialization
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-           string EDPString= Request.QueryString["id"];
-           Response.Write(EDPString);
+            string EDPString = Request.QueryString["id"];
+            Response.Write(EDPString + "</br>");
+            string URL = "http://afs-sl-pservice01.afservice.org:8080/productservice2/getProductInfo/pcmall?edplist=";
+            URL += EDPString;
+            URL += "&ignoreCatalog=true";
+            Response.Write(URL);
+            XmlTextReader reader = new XmlTextReader(URL);
+            reader.WhitespaceHandling = WhitespaceHandling.Significant;
+            while (reader.ReadToFollowing("result"))
+            {
+                while (reader.ReadToFollowing("productDetails"))
+                {
+                    string valuetext = reader.Value;
+
+                    Response.Write("Value:" + valuetext);
+                    Response.Write("</br>");
+
+                    //Response.Write("Attribute Name: " + attr);
+                    //Response.Write("</br>");
+                }
+            }
         }
     }
 }
