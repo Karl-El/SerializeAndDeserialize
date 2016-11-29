7fcf2f61f7f2792fc6370da219b4e6326006af80
KarlKrus <carl_son_cruz@live.com>
2016-11-29 10:41:11 +0800

    11.29.16.10.40.AM

--- WebServiceDeserialization/Result.aspx.cs
+++ WebServiceDeserialization/Result.aspx.cs
@@ -12,20 +12,20 @@ namespace WebServiceDeserialization
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            XmlReader xmlReader = XmlReader.Create("http://afs-sl-schmgr03.afservice.org:8080/searchManager/search/afs-sl-schmstr.afservice.org:8080/solr4/Products/select?q=laptop&fl=EDP&store=pcmall&rows=25&start=0&facet=true&facet.field=Manufacturer&facet.field=InStock&facet.limit=10");
-            while (xmlReader.Read())
-            {
-                if ((xmlReader.NodeType == XmlNodeType.Element) && (xmlReader.Name == "response"))
-                {
-                    if (xmlReader.HasAttributes)
-                    {
-                        //Response.Write(xmlReader.GetAttribute("currency") + ": " + xmlReader.GetAttribute("rate"));
-                        string valuetext = xmlReader.ReadElementString("int");
-                        Response.Write("Value:" + valuetext);
-                        Response.Write("</br>");
-                    }
-                }
-            }
+            //XmlReader xmlReader = XmlReader.Create("http://afs-sl-schmgr03.afservice.org:8080/searchManager/search/afs-sl-schmstr.afservice.org:8080/solr4/Products/select?q=laptop&fl=EDP&store=pcmall&rows=25&start=0&facet=true&facet.field=Manufacturer&facet.field=InStock&facet.limit=10");
+            //while (xmlReader.Read())
+            //{
+            //    if ((xmlReader.NodeType == XmlNodeType.Element) && (xmlReader.Name == "response"))
+            //    {
+            //        if (xmlReader.HasAttributes)
+            //        {
+            //            //Response.Write(xmlReader.GetAttribute("currency") + ": " + xmlReader.GetAttribute("rate"));
+            //            string valuetext = xmlReader.ReadElementString("int");
+            //            Response.Write("Value:" + valuetext);
+            //            Response.Write("</br>");
+            //        }
+            //    }
+            //}
 
             /* //------------------------------------------------------------------------------------------ NOT WORKING READER
              XmlTextReader reader = new XmlTextReader("http://afs-sl-schmgr03.afservice.org:8080/searchManager/search/afs-sl-schmstr.afservice.org:8080/solr4/Products/select?q=laptop&fl=EDP&store=pcmall&rows=25&start=0&facet=true&facet.field=Manufacturer&facet.field=InStock&facet.limit=10");
@@ -45,22 +45,24 @@ namespace WebServiceDeserialization
              } */
 
 
-            /*//-----------------------------------------------------------------------------------------------------WORKING READER
+            //-----------------------------------------------------------------------------------------------------WORKING READER
             XmlTextReader reader = new XmlTextReader("http://afs-sl-schmgr03.afservice.org:8080/searchManager/search/afs-sl-schmstr.afservice.org:8080/solr4/Products/select?q=laptop&fl=EDP&store=pcmall&rows=25&start=0&facet=true&facet.field=Manufacturer&facet.field=InStock&facet.limit=10");
             reader.WhitespaceHandling = WhitespaceHandling.Significant;
-            while (reader.ReadToFollowing("int"))
+            while (reader.ReadToFollowing("result"))
             {
-                string attr = reader.GetAttribute("name");
-                string valuetext = reader.ReadElementString("int");
+                while (reader.ReadToFollowing("int"))
+                {
+                    string attr = reader.GetAttribute("name");
+                    string valuetext = reader.ReadElementString("int");
 
-                Response.Write("Attribute Name: " + attr);
-                Response.Write("</br>");
-                Response.Write("Value:" + valuetext);
-                Response.Write("</br>");
-                Response.Write("</br>");
-
-            }*/
-
+                    Response.Write("Attribute Name: " + attr);
+                    Response.Write("</br>");
+                    Response.Write("Value:" + valuetext);
+                    Response.Write("</br>");
+                    Response.Write("</br>");
+                }
+            }
         }
+
     }
 }
