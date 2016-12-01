d581275a9716eb1a89aef8121ceab3c100d9e419
KarlKrus <carl_son_cruz@live.com>
2016-12-01 14:59:07 +0800

    12.01.16.02.58.PM

--- WebServiceDeserialization/Result.aspx.cs
+++ WebServiceDeserialization/Result.aspx.cs
@@ -12,6 +12,7 @@ namespace WebServiceDeserialization
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            #region
             //XmlReader xmlReader = XmlReader.Create("http://afs-sl-schmgr03.afservice.org:8080/searchManager/search/afs-sl-schmstr.afservice.org:8080/solr4/Products/select?q=laptop&fl=EDP&store=pcmall&rows=25&start=0&facet=true&facet.field=Manufacturer&facet.field=InStock&facet.limit=10");
             //while (xmlReader.Read())
             //{
@@ -43,11 +44,18 @@ namespace WebServiceDeserialization
                  reader.ReadEndElement();
                  reader.Read();
              } */
-
-
+            #endregion
+            string EDP = "";
+            System.Web.UI.WebControls.Label lbl_Store = (System.Web.UI.WebControls.Label)_rptrEDP.FindControl("lbl_Store");
+            System.Web.UI.WebControls.Label lbl_Name = (System.Web.UI.WebControls.Label)_rptrEDP.FindControl("lbl_Name");
+            System.Web.UI.WebControls.Label lbl_Description = (System.Web.UI.WebControls.Label)_rptrEDP.FindControl("lbl_Description");
+            System.Web.UI.WebControls.Label lbl_Price = (System.Web.UI.WebControls.Label)_rptrEDP.FindControl("lbl_Price");
+            System.Web.UI.WebControls.Label lbl_Manufacturer = (System.Web.UI.WebControls.Label)_rptrEDP.FindControl("lbl_Manufacturer");
+            System.Web.UI.WebControls.Label lbl_Availability = (System.Web.UI.WebControls.Label)_rptrEDP.FindControl("lbl_Availability");
+            System.Web.UI.WebControls.Image img_Prod = (System.Web.UI.WebControls.Image)_rptrEDP.FindControl("img_Prod");
             //-----------------------------------------------------------------------------------------------------WORKING READER
             List<int> saveEDP = new List<int>();
-            XmlTextReader reader = new XmlTextReader("http://afs-sl-schmgr03.afservice.org:8080/searchManager/search/afs-sl-schmstr.afservice.org:8080/solr4/Products/select?q=laptop&fl=EDP&store=pcmall&rows=40&start=0");
+            XmlTextReader reader = new XmlTextReader("http://afs-sl-schmgr03.afservice.org:8080/searchManager/search/afs-sl-schmstr.afservice.org:8080/solr4/Products/select?q=laptop&fl=EDP&store=pcmall&rows=2&start=0");
             reader.WhitespaceHandling = WhitespaceHandling.Significant;
             while (reader.ReadToFollowing("result"))
             {
@@ -59,7 +67,8 @@ namespace WebServiceDeserialization
                         string valuetext = reader.ReadElementString("int");
 
                         saveEDP.Add(Convert.ToInt32(valuetext));
-
+                        EDP = valuetext.ToString();
+                      //  ProdDetail.showDetails(EDP, lbl_Store, lbl_Name, lbl_Description, lbl_Price, img_Prod, lbl_Manufacturer,lbl_Availability);
                         //Response.Write("Value:" + valuetext);
                         //Response.Write("</br>");
 
@@ -69,9 +78,10 @@ namespace WebServiceDeserialization
                 }
 
             }
+            ProdDetail.showDetails(EDP, lbl_Store, lbl_Name, lbl_Description, lbl_Price, img_Prod,lbl_Manufacturer,lbl_Availability);
             _rptrEDP.DataSource = saveEDP;
             _rptrEDP.DataBind();
         }
-        
+
     }
 }
