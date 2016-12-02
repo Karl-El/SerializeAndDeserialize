16d84ba21077af5a23a0edff7e058f8fa23f87a5
KarlKrus <carl_son_cruz@live.com>
2016-12-02 13:35:46 +0800

    12.02.16.01.35.PM

--- WebServiceDeserialization/ProductDetails.aspx.cs
+++ WebServiceDeserialization/ProductDetails.aspx.cs
@@ -15,83 +15,89 @@ namespace WebServiceDeserialization
 
             string EDPString = Request.QueryString["id"];
             Response.Write("EDP: " + EDPString + "</br>");
-            string URL = "http://afs-sl-pservice01.afservice.org:8080/productservice2/getProductInfo/pcmall?edplist=";
-            URL += EDPString;
-            URL += "&ignoreCatalog=true";
+            string URL = "http://afs-sl-pservice01.afservice.org:8080/productservice2/getProductInfo/pcmall?edplist="+ EDPString + "&ignoreCatalog=true";
             XmlTextReader reader = new XmlTextReader(URL);
-            reader.WhitespaceHandling = WhitespaceHandling.Significant;
+            //reader.WhitespaceHandling = WhitespaceHandling.Significant;
             string DetailString = "";
-            while (reader.ReadToFollowing("productService"))
+            #region WHILES
+            //while (reader.ReadToFollowing("productService"))
+            //{
+            //    while (reader.ReadToFollowing("getProductInfo"))
+            //    {
+            //        while (reader.ReadToFollowing("result"))
+            //        {
+
+            //            while (reader.ReadToFollowing("item"))
+            //            {
+
+            //                while (reader.ReadToFollowing("productDetails"))
+            //                #region productDetailsNODE
+            //                {
+
+            //                }
+            //                #endregion
+            //                //while (reader.ReadToFollowing("prices"))
+            //                //#region pricesNODE
+            //                //{
+            //                //    if (reader.Name == "finalPrice")
+            //                //    {
+            //                //        DetailString += "Final Price: ";
+            //                //        DetailString += reader.ReadElementString("finalPrice");
+            //                //        DetailString += "</br>";
+            //                //    }
+            //                //}
+            //                //#endregion
+            //            }
+            //        }
+            //    }
+            //}
+            #endregion
+
+            while (reader.Read())
             {
-                while (reader.ReadToFollowing("getProductInfo"))
+                if (reader.Name == "name")
                 {
-                    while (reader.ReadToFollowing("result"))
-                    {
-
-                        while (reader.ReadToFollowing("item"))
-                        {
-
-                            while (reader.ReadToFollowing("productDetails"))
-                            #region
-                            {
-                                while (reader.Read())
-                                {
-                                    if (reader.Name == "name")
-                                    {
-                                        DetailString += "Name:";
-                                        DetailString += reader.ReadElementString("name");
-                                        DetailString += "</br>";
-                                    }
-                                    if (reader.Name == "manufacturer")
-                                    {
-                                        DetailString += "Manufacturer:";
-                                        DetailString += reader.ReadElementString("manufacturer");
-                                        DetailString += "</br>";
-                                    }
-                                    if (reader.Name == "description")
-                                    {
-                                        DetailString += "Desc:";
-                                        DetailString += reader.ReadElementString("description");
-                                        DetailString += "</br>";
-                                    }
-                                    //if (reader.Name == "store")
-                                    //{
-                                    //    valuetext += "Store:";
-                                    //    valuetext += reader.ReadElementString("store");
-                                    //    valuetext += "</br>";
-                                    //}
-                                    //if (reader.Name == "availabilityDescription")
-                                    //{
-                                    //    valuetext += "Availabilty:";
-                                    //    valuetext += reader.ReadElementString("availabilityDescription");
-                                    //    valuetext += "</br>";
-                                    //}
-                                    //if (reader.Name == "xlg")
-                                    //{
-                                    //    valuetext += "Image URL:";
-                                    //    valuetext += reader.ReadElementString("xlg");
-                                    //    valuetext += "</br>";
-                                    //}
-                                }
-                            }
-                            #endregion
-
-                            while (reader.ReadToFollowing("prices"))
-                            #region
-                            {
-                                if (reader.Name == "finalPrice")
-                                {
-                                    DetailString += "Final Price:";
-                                    DetailString += reader.ReadElementString("finalPrice");
-                                    DetailString += "</br>";
-                                }
-                            }
-                            #endregion
-                        }
-                    }
+                    DetailString += "Name: ";
+                    DetailString += reader.ReadElementString("name");
+                    DetailString += "</br>";
+                }
+                if (reader.Name == "manufacturer")
+                {
+                    DetailString += "Manufacturer: ";
+                    DetailString += reader.ReadElementString("manufacturer");
+                    DetailString += "</br>";
+                }
+                if (reader.Name == "description")
+                {
+                    DetailString += "Description: ";
+                    DetailString += reader.ReadElementString("description");
+                    DetailString += "</br>";
+                }
+                if (reader.Name == "finalPrice")
+                {
+                    DetailString += "Final Price: ";
+                    DetailString += reader.ReadElementString("finalPrice");
+                    DetailString += "</br>";
+                }
+                if (reader.Name == "store")
+                {
+                    DetailString += "Store: ";
+                    DetailString += reader.ReadElementString("store");
+                    DetailString += "</br>";
+                }
+                if (reader.Name == "availabilityDescription")
+                {
+                    DetailString += "Availabilty: ";
+                    DetailString += reader.ReadElementString("availabilityDescription");
+                    DetailString += "</br>";
+                }
+                if (reader.Name == "xlg")
+                {
+                    DetailString += "Image URL: ";
+                    DetailString += reader.ReadElementString("xlg");
+                    DetailString += "</br>";
                 }
             }
-
             Response.Write(DetailString);
             Response.Write("</br>");
         }
