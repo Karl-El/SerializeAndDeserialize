425a354494aa070587da9da246d4930cb414481c
KarlKrus <carl_son_cruz@live.com>
2016-12-07 09:46:17 +0800

    12.07.16.09.46.AM

--- WebServiceDeserialization/EDPbyBrand.cs
+++ WebServiceDeserialization/EDPbyBrand.cs
@@ -2,15 +2,48 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Xml;
 
 namespace WebServiceDeserialization
 {
     public class EDPbyBrand
     {
-        public List<string> ListingBrandEDP()
+        EDPList EDPList = new EDPList();
+        public List<string> ListingBrandEDP(string ManufactSelect)
         {
+            string EDPforBrand = "";
+            string ReadManufact = "";
+            List<string> ListEDP;
+            ListEDP = EDPList.ListingEDP();
             List<string> BrandEDPs = new List<string>();
-
+            for (int i = 0; i < ListEDP.Count; i++)
+            {
+                #region FOR START READ A SPECIFIC BRAND/MANUFACTURER
+                {
+                    string URL = "http://afs-sl-pservice01.afservice.org:8080/productservice2/getProductInfo/pcmall?edplist=" + ListEDP[i] + "&ignoreCatalog=true";
+                    XmlTextReader reader = new XmlTextReader(URL);
+                    reader.WhitespaceHandling = WhitespaceHandling.Significant;
+                    while (reader.Read())
+                    {
+                        if (reader.Name == "edp")
+                        {
+                            EDPforBrand = reader.ReadElementString("edp");
+                        }
+                        if (reader.Name == "manufacturer")
+                        {
+                            ReadManufact = reader.ReadElementString("manufacturer");
+                            if (ReadManufact == ManufactSelect)
+                            {
+                                string EDPSTRINGLISTBRANDED = "";
+                                BrandEDPs.Add(EDPforBrand);
+                                EDPSTRINGLISTBRANDED += EDPforBrand;
+                                break;
+                            }
+                        }
+                    }
+                }
+                #endregion
+            }
             return (BrandEDPs);
         }
     }
